95d560aec6975e988b9fb48163ae7906cbcaed68
cookeem <cookeem@qq.com>
2017-05-09 15:28:43 +0800

    jenkins+docker+gitlab持续集成测试

--- test/equal_test.go
+++ test/equal_test.go
@@ -8,7 +8,7 @@ func TestEqual(t *testing.T) {
 	a := 1
 	b := 1
 	shouldBe := true
-	if real := equal(a, b); real == shouldBe {
+	if real := equal(a, b); real != shouldBe {
 		t.Errorf("equal(%d, %d) should be %v, but is:%v\n", a, b, shouldBe, real)
 	}
 }
