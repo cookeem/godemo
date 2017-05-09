6d6b0f7ce9954a3297454ba47f7c9268d4abf41e
cookeem <cookeem@qq.com>
2017-05-09 12:16:56 +0800

    jenkins+docker+gitlab持续集成测试

--- gin/gin_demo.go
+++ gin/gin_demo.go
@@ -17,13 +17,13 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Usage: /user/name or /user/name/action<br> App Version is: %s\n", VersionName)
+		c.String(http.StatusOK, "Usage: /user/name or /user/name/action\n App Version is: %s", VersionName)
 	})
 
 	// This handler will match /user/john but will not match neither /user/ or /user
 	router.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
-		c.String(http.StatusOK, "Hello %s<br> App Version is: %s\n", name, VersionName)
+		c.String(http.StatusOK, "Hello %s\n App Version is: %s", name, VersionName)
 	})
 
 	// However, this one will match /user/john/ and also /user/john/send
@@ -32,7 +32,7 @@ func main() {
 		name := c.Param("name")
 		action := c.Param("action")
 		message := name + " is " + action
-		c.String(http.StatusOK, "%s<br> App Version is: %s\n", message, VersionName)
+		c.String(http.StatusOK, "%s\n App Version is: %s", message, VersionName)
 	})
 
 	router.Run(":8081")
