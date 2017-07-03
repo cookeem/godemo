1a6dfb693e15e052d00b5a21c777b6d852f53e67
cookeem <cookeem@qq.com>
2017-07-03 16:30:41 +0800

    new file:   ch3/channel_demo4.go

---
+++ ch3/channel_demo4.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	// channel传递
+	cht1 := make(chan int)
+	cht2 := make(chan int)
+	go func() {
+		time.Sleep(time.Second * 1)
+		fmt.Println(time.Now(), "cht1")
+		cht1 <- 1
+		return
+	}()
+	go func() {
+		i := <- cht1
+		time.Sleep(time.Second * 1)
+		i ++
+		fmt.Println(time.Now(), "cht2", i)
+		cht2 <- i
+		return
+	}()
+	i := <- cht2
+	fmt.Println(time.Now(), "cht2 finish:", i)
+
+
+}
